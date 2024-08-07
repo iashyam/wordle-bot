1974b82c3711a59e96e06a8c4086f83368e65710
iashyam <shyam10kwd@gmail.com>
2024-08-07 21:39:34 +0530

    beautifies the main file

--- wordle_bot.py
+++ wordle_bot.py
@@ -1,63 +1,12 @@
 import pandas as pd
 from numpy import log2 as ln
 from tqdm import tqdm
+from src import *
 
 class wordle:
-	def __init__(self):
-		self.get_words()
-		self.patterns = self.get_patterns()
-		df = pd.read_csv("frequencies.csv")
-		self.df = df.set_index('word')
-
-	def get_words(self):
-		data = pd.read_csv("wordle-allowed-guess.csv")
-		# data = pd.read_csv("allowed_words.txt")
-
-		self.words = data["word"].to_list()
-
 	def get_probabilities(self, thing):
 		return self.df.loc[thing]['prob'].sum()
-
-	#create a list of patterns
-	def get_patterns(self):
-		lst = [""]
 		
-		for word in lst:
-			if len(word)<5:
-				lst.append(word+"B")
-				lst.append(word+"G")
-				lst.append(word+"Y")
-			else:
-				continue
-
-		list2 = []
-		for word in lst:
-			if len(word)==5:
-				list2.append(word)
-
-		return list2
-
-	#compare two words and get pattern
-	def compare_words(self, check, ref):
-		check = check.upper()
-		ref = ref.upper()
-
-		pattern = ""
-		for i in range(5):
-			letter = check[i]
-			if letter not in ref:
-				pattern += "B"
-			elif letter in ref:
-				if ref[i]==letter:
-					pattern += "G"
-				else:
-					if letter in check[:i]:
-						pattern += "B"
-					else:
-						pattern += "Y"
-		return pattern 
-
-
 		#get the next most probable words:
 	def max_info(self, word_list):
 		infos = {}
@@ -67,11 +16,11 @@ class wordle:
 			#comparing each words and sorting the patterns
 		for choice in word_list:
 			my_dict = {}
-			for i in range(len(self.patterns)):
-				my_dict[self.patterns[i]]=[]
+			for i in range(len(Data.patterns)):
+				my_dict[Data.patterns[i]]=[]
 
 			for ref in word_list:
-				pattern = self.compare_words(choice, ref)
+				pattern = Func.compare_words(choice, ref)
 				my_dict[pattern].append(ref)
 
 			self.dicts.append(my_dict)
@@ -100,7 +49,7 @@ class wordle:
 		print(infos_df.head())
 	
 	def solve(self):
-		words = self.words
+		words = Data.words
 		for i in range(6):
 			infos = self.max_info(words)
 			self.print_info(infos)
